81bdd683f635d3ef47db351ebd4180ae5d65cd30
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-05 01:52:17 +0900

    Update lottery-container.js

--- assets/javascripts/discourse/connectors/post-bottom/lottery-container.js
+++ assets/javascripts/discourse/connectors/post-bottom/lottery-container.js
@@ -1,25 +1,24 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import { inject as service } from "@ember/service";
 
 export default class LotteryContainer extends Component {
-  @service site;
-  @tracked lotteryData = null;
-
-  constructor() {
-    super(...arguments);
-    this.loadLotteryData();
+  // this.args.outletArgs.model 是从插件出口传递过来的 Post 对象
+  get post() {
+    return this.args.outletArgs.model;
+  }
+  
+  // 从 Post 对象中获取 Topic 数据
+  get topic() {
+    return this.post?.topic;
   }
 
+  // 从 Topic 数据中获取我们的抽奖数据
+  get lotteryData() {
+    return this.topic?.lottery;
+  }
+
+  // 这是决策逻辑：当且仅当这是第一个帖子且包含抽奖数据时，才显示组件
   get shouldDisplay() {
-    const post = this.args.outletArgs.model;
-    return post?.post_number === 1 && this.lotteryData;
-  }
-
-  loadLotteryData() {
-    const topic = this.args.outletArgs.model?.topic;
-    if (topic?.lottery) {
-      this.lotteryData = topic.lottery;
-    }
+    return this.post?.post_number === 1 && this.lotteryData;
   }
 }
